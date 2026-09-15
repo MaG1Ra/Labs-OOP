5bc953ec203fb2d638d8b07235063787fe61a991
MaG1Ra <gindulin_06@mail.ru>
2026-09-15 17:10:13 +0500

    Добавил функцию reSizeArray, с помощью которой можно изменять размеры массива

--- Lab1/Task3.cpp
+++ Lab1/Task3.cpp
@@ -24,7 +24,29 @@ int& getElement(SafeArray& arr, int index) {
 
 void printSafe(const SafeArray& arr) {
     for (int i = 0; i < arr.size; i++) {
-        std::cout << arr.data[i];
+        std::cout << arr.data[i] << " | ";
+    }
+}
+
+void reSizeArray(int N, int M, SafeArray& arr) {
+    if (N == M) {
+        return;
+    }
+    int* new_data = new int[M]{};
+    if (N > M) {
+        for (int i = M; i < N; i++) {
+            std::cout << arr.data[i] << " | ";
+        }
+        std::copy(arr.data, arr.data + M, new_data);
+        delete[] arr.data;
+        arr.data = new_data;
+        arr.size = M;
+    }
+    if (N < M) {
+        std::copy(arr.data, arr.data + N, new_data);
+        delete[] arr.data;
+        arr.data = new_data;
+        arr.size = M;
     }
 }
 
@@ -34,5 +56,15 @@ int main() {
     getElement(MyArr, 2) = 999;
     std::cout << MyArr.data[2] << "\n";
     printSafe(MyArr);
+    reSizeArray(5, 10, MyArr);
+    std::cout << "\n";
+    getElement(MyArr, 6) = 666;
+    printSafe(MyArr);
+    std::cout << "\n";
+    reSizeArray(10, 3, MyArr);
+    std::cout << "\n";
+    printSafe(MyArr);
+    delete[] MyArr.data;
+    MyArr.data = nullptr;
     return 0;
 }
