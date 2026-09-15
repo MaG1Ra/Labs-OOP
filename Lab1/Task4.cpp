3e044209943222886ed018e8161450b007e91744
MaG1Ra <gindulin_06@mail.ru>
2026-09-15 17:59:03 +0500

    Добавил функцию fillMatrix, которая нужная для заполнения матрицы

--- Lab1/Task4.cpp
+++ Lab1/Task4.cpp
@@ -8,7 +8,14 @@ int** allocateMatrix(int rows, int cols) {
     return matrix;
 }
 
-
+void fillMatrix(int** matrix, int rows, int cols) {
+    srand(time(NULL));
+    for (int i = 0; i < rows;i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = rand() % 4 + 2;
+        }
+    }
+}
 
 int main() {
     int rows{};
