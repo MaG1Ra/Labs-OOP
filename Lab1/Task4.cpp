f7202dc18e6012787633e25ac04c24bcefa96292
MaG1Ra <gindulin_06@mail.ru>
2026-09-15 19:20:04 +0500

    Добавил функцию printMatrix для возмиожности красиво выводить рамку матрицы, с возможностью простого вывода матрицы

--- Lab1/Task4.cpp
+++ Lab1/Task4.cpp
@@ -17,7 +17,42 @@ void fillMatrix(int** matrix, int rows, int cols) {
     }
 }
 
+void printMatrix(int** matrix, int rows, int cols, bool showBorders = true, std::string title = "Matrix"){
+    std::cout << title << "\n";
+    if(showBorders){
+        for(int i = 0; i < cols + 5; i++){
+            std::cout << "*";
+        }
+        std::cout << "\n";
+    }
+
+    for(int i = 0; i < rows; i++){
+        if(showBorders){
+            std::cout << "*";
+        }
+        for(int j = 0; j < cols; j++){
+            std::cout << matrix[i][j];
+            if(j != cols - 1){
+                std::cout << " ";
+            }
+        }
+        if(showBorders){
+            std::cout << "*";
+        }
+        std::cout << "\n";
+    }
+
+    if(showBorders){
+        for(int i = 0; i < cols + 5; i++){
+            std::cout << "*";
+        }
+    }
+}
+
 int main() {
-    int rows{};
-    int cols{};
+    int rows{4};
+    int cols{4};
+    int** Matrix{allocateMatrix(rows, cols)};
+    fillMatrix(Matrix, rows, cols);
+    printMatrix(Matrix, rows, cols, true, "Grades");
 }
